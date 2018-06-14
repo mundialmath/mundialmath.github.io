7421248ccbf64d253cbd70df009a9c1ed6d62ac3
nagato <hsebasa@gmail.com>
2018-06-13 23:48:52 -0500

    Revert "Commit a medias"
    This reverts commit 0cc9932

--- app/source/resources/mundialApi.js
+++ app/source/resources/mundialApi.js
@@ -1,5 +1,5 @@
 angular.module('mundialMath.resources', [])
-    .factory('userApi',  ['$http',  function($http){
+    .factory('mundialApi',  ['$http',  function($http){
         let baseUrl = 'http://127.0.0.1:5000';
         return {
             getUsers: function(){
@@ -19,19 +19,6 @@ angular.module('mundialMath.resources', [])
                 //     ]
                 // }
             },
-
-            enviarOdds: function(odds){
-                $http({
-                    url: baseUrl+'/users/'+odds['usuario']+'/',
-                    method: 'POST',
-                    params: odds,
-                    crossDomain: true
-                })
-            }
-        }
-    }])
-    .factory('userInputApi',  ['$http',  function($http){
-        return {
             getInputs: function(userName){
                 return  $http({
                     url: baseUrl+'/users/'+userName,
@@ -48,12 +35,7 @@ angular.module('mundialMath.resources', [])
                 //         ['Camilo', 4]
                 //     ]
                 // }
-            }
-        }
-    }
-    ])
-    .factory('partidosApi',  ['$http',  function($http){
-        return {
+            },
             getPartidos: function(){
                 return  $http({
                     url: baseUrl+'/partidos',
@@ -74,6 +56,15 @@ angular.module('mundialMath.resources', [])
                 //         ['ddfda3', 4, 5]
                 //     ]
                 // }
+            },
+
+            enviarOdds: function(odds){
+                $http({
+                    url: baseUrl+'/users/'+odds['usuario']+'/',
+                    method: 'POST',
+                    params: odds,
+                    crossDomain: true
+                })
             }
         }
     }
