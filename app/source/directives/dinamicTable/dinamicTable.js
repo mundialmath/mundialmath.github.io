d73fa8081372643688db1322449d67fb58289d6b
nagato <hsebasa@gmail.com>
2018-06-17 22:26:45 -0500

    Se crea el filtro para identificar columnas por nombre

--- app/source/directives/dinamicTable/dinamicTable.js
+++ app/source/directives/dinamicTable/dinamicTable.js
@@ -4,10 +4,12 @@
 angular.module('mundialMath.directives')
     .directive('dinamicTable', ['$timeout', function($timeout){
         function link($scope, $element){
+            $scope.showColumns = (typeof $scope.showColumns !== "undefined") ? $scope.showColumns: $scope.table.columns;
         }
         return {
             scope: {
                 table: '=',
+                showColumns: '=?'
             },
             link: link,
             templateUrl: 'directives/dinamicTable/templateDinamicTable.html'
