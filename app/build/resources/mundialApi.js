eb946bd0dc0a69a50f776f84d892537553263bdd
nagato <hsebasa@gmail.com>
2018-06-18 22:08:14 -0500

    Se actualiza la url remota de heroku

--- app/build/resources/mundialApi.js
+++ app/build/resources/mundialApi.js
@@ -1,6 +1,7 @@
 angular.module('mundialMath.resources')
     .factory('mundialApi',  ['$http',  function($http){
-        var baseUrl = 'http://127.0.0.1:5000';
+        // var baseUrl = 'http://127.0.0.1:5000';
+        var baseUrl = 'https://fierce-waters-19600.herokuapp.com';
         return {
             getUsers: function(){
                 return  $http({
