e3fe7811ac5f356f5ea869cff0895472ac088344
nagato <hsebasa@gmail.com>
2018-06-21 15:23:41 -0500

    Ok

--- app/build/modificarResultados/controller.js
+++ app/build/modificarResultados/controller.js
@@ -9,10 +9,7 @@ angular.module('mundialMath.modificarResultados', ['ngRoute', 'mundialMath.resou
     }])
     .controller('modificarResultados', ['mundialApi', 'utilities', '$scope', '$timeout',
         function($mundialApi, $utilities, $scope, $timeout) {
-            $scope.tablaStr = '';
             $scope.tablaPartidos = null;
-            $scope.columnIndex = $utilities.columnIndex;
-            $scope.urlTabla = '';
 
             $scope.getPartidos = function(){
                 $mundialApi.getPartidosHistorico()
@@ -26,24 +23,6 @@ angular.module('mundialMath.modificarResultados', ['ngRoute', 'mundialMath.resou
             $scope.guardarCambios = function(){
                 $mundialApi.guardarResultadosHistoricos($scope.tablaPartidos);
             };
-
-            $scope.addRow = function(){
-                var row = [];
-                for (var i=0; i<$scope.tablaPartidos.columns.length; i++){
-                    row.push('')
-                }
-                $scope.tablaPartidos.data.push(row)
-            };
-
-            $scope.deleteRow = function(i){
-                $scope.tablaPartidos.data.splice(i, 1);
-            };
-
-            $scope.$watch('tablaPartidos.data', function() {
-                var content = $utilities.table2str($scope.tablaPartidos);
-                var blob = new Blob([ content ], { type : 'text/plain' });
-                $scope.urlTabla = (window.URL || window.webkitURL).createObjectURL( blob );
-            }, true);
 
 
             (function init(){
--- app/build/modificarUsuarios/controller.js
+++ app/build/modificarUsuarios/controller.js
@@ -9,10 +9,7 @@ angular.module('mundialMath.modificarUsuarios', ['ngRoute', 'mundialMath.resourc
     }])
     .controller('modificarUsuarios', ['mundialApi', 'utilities', '$scope', '$timeout',
         function($mundialApi, $utilities, $scope, $timeout) {
-            $scope.tablaStr = '';
             $scope.tablaPartidos = null;
-            $scope.columnIndex = $utilities.columnIndex;
-            $scope.urlTabla = '';
 
             $scope.getPartidos = function(){
                 $mundialApi.getUsers()
@@ -25,24 +22,6 @@ angular.module('mundialMath.modificarUsuarios', ['ngRoute', 'mundialMath.resourc
 
             $scope.guardarCambios = function(){
                 $mundialApi.guardarResultadosHistoricos($scope.tablaPartidos);
-            };
-
-            $scope.$watch('tablaPartidos.data', function() {
-                var content = $utilities.table2str($scope.tablaPartidos);
-                var blob = new Blob([ content ], { type : 'text/plain' });
-                $scope.urlTabla = (window.URL || window.webkitURL).createObjectURL( blob );
-            }, true);
-
-            $scope.addRow = function(){
-                var row = [];
-                for (var i=0; i<$scope.tablaPartidos.columns.length; i++){
-                    row.push('')
-                }
-                $scope.tablaPartidos.data.push(row)
-            };
-
-            $scope.deleteRow = function(i){
-                $scope.tablaPartidos.data.splice(i, 1);
             };
 
             (function init(){
